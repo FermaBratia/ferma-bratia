55e8e515bbf581937aa20fb2938c6d3127ec50f3
Stefan Tampu <stefan_tampu@yahoo.com>
2023-09-13 01:04:46 +0300

    Add extra phone number in footer and update h1 font Phone numbers animated through css and js

--- bratia.js
+++ bratia.js
@@ -2,6 +2,29 @@ const header = document.querySelector("header");
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector("nav");
 const navLink = document.querySelectorAll(".nav-link");
+const telefonFermaBratia = document.querySelectorAll(".telefon-ferma-bratia");
+
+telefonFermaBratia.forEach(telNumber => {
+    telNumber.addEventListener("mouseover", () => {
+        if (telNumber.id === "madalina-number") {
+            telNumber.textContent = "Madalina";
+        } else {
+            telNumber.textContent = "Dani";
+        }
+    })
+    telNumber.addEventListener("mouseout", () => {
+        if (telNumber.id === "madalina-number") {
+            telNumber.textContent = "0753340618";
+        } else {
+            telNumber.textContent = "0752520282";
+        }
+    })
+});
+addEventListener("hover", phoneName);
+
+function phoneName() {
+    telefonFermaBratia.text
+}
 
 hamburger.addEventListener("click", mobileMenu);
 
