df0b7fe5102b3418d9071789e78a429edf9eedac
Stefan Tampu <stefan_tampu@yahoo.com>
2023-10-17 14:46:50 +0300

    Update index and products Index despre-noi section has gallery with images changeImg() function in js changes images Update product pages Updated images, updated buttons & weights, updated prices Add capaceala page

--- bratia.js
+++ bratia.js
@@ -12,38 +12,6 @@ const imagesImg = document.getElementById("images-img");
 
 //Hamburger Menu Section
 
-let img=1;
-let imageH2 = document.createElement("h2");
-imageH2.textContent = "Următorul apicultor al familiei";
-imagesText.appendChild(imageH2);
-
-function changeImg() {
-    if (img===1){
-        imageH2.textContent = "Următorul apicultor al familiei";
-        imagesImg.classList = "";
-        imagesImg.classList.add("i1");
-    } else if (img===2){
-        imageH2.textContent = "";
-        imagesImg.classList = "";
-        imagesImg.classList.add("i2");
-    };
-}
-
-btnLeft.addEventListener("click", () => {
-    if (img>1){
-        img--;
-    }
-    changeImg();
-});
-
-btnRight.addEventListener("click", () => {
-    img++;
-    changeImg();
-})
-
-
-// btnLeft.addEventListener("click", nah);
-
 function mobileMenu() {
     hamburger.classList.toggle("active");
     nav.classList.toggle("active");
@@ -60,44 +28,117 @@ navLinks.forEach(navLink => {
     navLink.addEventListener("click", closeMenu);
 })
 
+//Image Gallery Section
+
+if (imagesText){
+    let img=1;
+    let imageH2 = document.createElement("h2");
+    imageH2.textContent = "Următorul apicultor al familiei";
+    imagesText.appendChild(imageH2);
+
+function changeImg() {
+    if (img===1){
+        imageH2.textContent = "Următorul apicultor al familiei";
+        imagesImg.classList = "";
+        imagesImg.classList.add("i1");
+    } else if (img===2){
+        imageH2.textContent = "Cu stupii în pastoral";
+        imagesImg.classList = "";
+        imagesImg.classList.add("i2");
+    } else if (img===3){
+        imageH2.textContent = "Stupina noastră";
+        imagesImg.classList = "";
+        imagesImg.classList.add("i3");
+    } else if (img===4){
+        imageH2.textContent = "Dani la muncă";
+        imagesImg.classList = "";
+        imagesImg.classList.add("i4");
+    } else if (img===5){
+        imageH2.textContent = "Mădălina și Dani";
+        imagesImg.classList = "";
+        imagesImg.classList.add("i5");
+    } else if (img===6){
+        imageH2.textContent = "Familia noastra de albine";
+        imagesImg.classList = "";
+        imagesImg.classList.add("i6");
+    }
+    ;
+}
+
+btnLeft.addEventListener("click", () => {
+    if (img>1) {
+        img--;
+    } else {
+        img = 6;
+    }
+    changeImg();
+});
+
+btnRight.addEventListener("click", () => {
+    if (img<6){
+        img++;
+    } else {
+        img = 1;
+    }
+    changeImg();
+})
+}
+
+
 //Product Button Section
 
 productButtons.forEach(productButton => {
     productButton.addEventListener("click", () => {
         productButtons.forEach(productButton => productButton.classList.remove("orange-button"));
         productButton.classList.add("orange-button");
-        if (productButton.id === "mana300"){
+        if (productButton.id === "mana450"){
             honeyPrice.textContent = "25 RON";
             honeyImage.id = "mana2";
-        } else if (productButton.id === "mana450"){
+        } else if (productButton.id === "mana1000"){
             honeyPrice.textContent = "45 RON";
             honeyImage.id = "mana";
-        } else if (productButton.id === "salcam300"){
+        } else if (productButton.id === "salcam450"){
             honeyPrice.textContent = "20 RON";
             honeyImage.id = "salcam2";
-        } else if (productButton.id === "salcam450"){
+        } else if (productButton.id === "salcam1000"){
             honeyPrice.textContent = "35 RON";
             honeyImage.id = "salcam";
         } //Pune floarea soarelui aici
         else if (productButton.id === "poliflora300"){
+            honeyPrice.textContent = "10 RON";
+            honeyImage.id = "poliflora3";
+        }
+        else if (productButton.id === "poliflora450"){
             honeyPrice.textContent = "15 RON";
             honeyImage.id = "poliflora2";
-        } else if (productButton.id === "poliflora450"){
+        } else if (productButton.id === "poliflora1000"){
             honeyPrice.textContent = "25 RON";
             honeyImage.id = "poliflora";
-        } else if (productButton.id === "rapita300"){
+        } else if (productButton.id === "rapita450"){
             honeyPrice.textContent = "15 RON";
             honeyImage.id = "rapita2";
-        } else if (productButton.id === "rapita450"){
+        } else if (productButton.id === "rapita1000"){
             honeyPrice.textConent = "25 RON";
             honeyImage.id = "rapita";
-        } else if (productButton.id === "tei300"){
+        } else if (productButton.id === "tei450"){
             honeyPrice.textContent = "15RON";
             honeyImage.id = "tei2";
-        } else if (productButton.id === "tei450"){
+        } else if (productButton.id === "tei1000"){
             honeyPrice.textContent = "30 RON";
             honeyImage.id = "tei";
-        } //Pune duo aici
+        } else if (productButton.id === "duo1000"){
+            honeyPrice.textContent = "30 RON";
+            honeyImage.id = "duo";
+        } else if (productButton.id === "duo300"){
+            honeyPrice.textContent = "15 RON";
+            honeyImage.id = "duo3";
+        } else if (productButton.id === "capaceala1000"){
+            honeyPrice.textContent = "35 RON";
+            honeyImage.id = "capaceala";
+        } else if (productButton.id === "capaceala450"){
+            honeyPrice.textContent = "20 RON";
+            honeyImage.id = "capaceala2";
+        }
         else if (productButton.id === "polen100"){
             honeyPrice.textContent = "10 RON";
             honeyImage.id = "polen2";
